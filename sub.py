2eba9960e806ff9686b2dd9858b16752c24e7102
Nitin Singh <nitin001.singh@gmail.com>
2020-07-05 19:58:55 +0530

    Changes in variable declaration

--- sub.py
+++ sub.py
@@ -1,4 +1,4 @@
-a = int(input("Enter First Number \t"))
-b = int(input("Enter Second Number \t"))
+x = int(input("Enter First Number \t"))
+y = int(input("Enter Second Number \t"))
 
 print(f"The Result is : {x} - {y} = {x-y}")
